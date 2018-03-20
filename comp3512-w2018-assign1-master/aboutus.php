1e58b6f9b000561ad5d7088d61da0735edb6c93f
John Hernandez <jhern922@mtroyal.ca>
2018-03-20 06:58:21 +0000

    Updated about-us page

--- comp3512-w2018-assign1-master/aboutus.php
+++ comp3512-w2018-assign1-master/aboutus.php
@@ -30,15 +30,38 @@
                   <p class="lead">This assignment was created by Lewis Furlan-Lowry and John Hernandez. It was created as the second assignment for COMP 3512.</p>
                       <hr class="my-4">
                         <h3 class="display-3">External Resources used</h3>
-                                        
-                                        
-                    <div class="panel panel-info">
+                        <div class="panel panel-info">
                         <ul class="list-group">
                             <li class="list-group-item"><a href="https://getbootstrap.com/">Bootstrap</a></li>
-                            <li class="list-group-item"><a href="https://github.com/rconnolly/comp3512-w2018-assign1">Project on GitHub</a></li>
+                            <li class="list-group-item"><a href="https://github.com/lewis-fl/lfurl515-jhern922-comp3512-assign2">Project on GitHub</a></li>
                             <li class="list-group-item"><a href="https://www.amazon.ca/Fundamentals-Web-Development-Randy-Connolly/dp/0133407152">Fundamentals of Web Development Textbook</a></li>
                         </ul>
-                    </div>
+                        </div>
+                    <hr class="my-4">
+                         <h3 class="display-3">Lewis Furlan-Lowry</h3>
+                        <div class="panel panel-info">
+                        <ul class="list-group">
+                            <li class="list-group-item">Gateway Classes(most)</li>
+                            <li class="list-group-item">User Profile</li>
+                            <li class="list-group-item">Login/Logout</li>
+                            <li class="list-group-item">Bootstrap Theme and Site Design</li>
+                            <li class="list-group-item">Single Country</li>
+                            <li class="list-group-item">Image Preview(Dynamic Image Preview)</li>
+                        </ul>
+                        </div>
+                    <hr class="my-4">
+                         <h3 class="display-3">John Hernandez</h3>
+                        <div class="panel panel-info">
+                        <ul class="list-group">
+                            <li class="list-group-item">Favorites</li>
+                            <li class="list-group-item">Single Post</li>
+                            <li class="list-group-item">Browse Posts</li>
+                            <li class="list-group-item">Simple Search/Search results</li>
+                            <li class="list-group-item">Single Image</li>
+                            <li class="list-group-item">Browse Images</li>
+                            <li class="list-group-item">Gateway Classes(some)</li>
+                        </ul>
+                        </div>
         </div>
         <!-- End jumbotron -->
       
